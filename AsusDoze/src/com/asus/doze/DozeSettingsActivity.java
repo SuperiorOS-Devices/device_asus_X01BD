9175e2ac053b4e0fd442ce889da941f3355ce45c
Bruno Martins <bgcngm@gmail.com>
2017-10-08 10:27:47 +0100

    X01BD: AsusDoze: Re-integrate into Settings
     * Apply the default Oreo theme and inherit the layouts
       from Google for the Settings app, in order to keep
       UI consistency.
    
     * Get rid of SettingsDrawerActivity as it no longer fits in
       and include the back button in the action bar.
    
     * Kill the icon drawable not only because is a leftover,
       but also doesn't really serve any puropose in the new UI.
    
    Change-Id: I53b6d3bb99d63852ba34d71e16ae99f05223569e

--- AsusDoze/src/com/asus/doze/DozeSettingsActivity.java
+++ AsusDoze/src/com/asus/doze/DozeSettingsActivity.java
@@ -19,11 +19,9 @@ package com.asus.doze;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.preference.PreferenceActivity;
 
-import com.android.settingslib.drawer.SettingsDrawerActivity;
-
-public class DozeSettingsActivity extends SettingsDrawerActivity {
+public class DozeSettingsActivity extends PreferenceActivity {
 
     private static final String TAG_DOZE = "doze";
 
@@ -31,19 +29,7 @@ public class DozeSettingsActivity extends SettingsDrawerActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.doze);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
                 new DozeSettingsFragment(), TAG_DOZE).commit();
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
