c9d69e7745d8be0b5b408ec7ca53f20384d014da
dianlujitao <dianlujitao@lineageos.org>
2017-12-07 22:15:14 +0800

    X01BD: Build dummy android.hidl.[base,manager]@1.0
    Change-Id: I81a081130869c4d92f30b2674e75bb6b096a7a2c

--- device.mk
+++ device.mk
@@ -47,3 +47,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
+
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0
