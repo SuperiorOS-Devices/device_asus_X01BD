bfd5e829fb3e070dd1ad0ae5e1fa5c6026086463
Rashed Abdel-Tawab <rashed@linux.com>
2018-04-08 17:01:00 +0000

    X01BD: Build a system version of android.hidl.base@1.0
    * dpmd fails to read it from vendor
    * Also build android.hidl.manager@1.0-java
    
    Change-Id: Icf4a29d6f8ef1517f4aa1971fc80a748632d8068

--- device.mk
+++ device.mk
@@ -258,7 +258,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0-java
 
 # IPv6
 PRODUCT_PACKAGES += \
