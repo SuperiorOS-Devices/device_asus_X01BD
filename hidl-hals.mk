791ff65c1b8703b723caa893b774c4ffc156ac1b
Michael Bestas <mkbestas@lineageos.org>
2018-05-27 05:18:54 +0300

    X01BD: sensors: Convert to X01BD target
    Change-Id: Ic7de84be4f5706d7a1ca2f210819bf8606165974

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -96,7 +96,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service.X01BD
 
 # Thermal
 PRODUCT_PACKAGES += \
