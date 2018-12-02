10c943fd45f3abe096362e1f63a710f99db765b3
kubersharma001 <kubersharma001@gmail.com>
2018-12-02 20:57:17 +0530

    X01BD: Initial commit
    Change-Id: I200148e998631f798d9410fc579983d4579d935a
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

---
+++ havoc_X01BD.mk
@@ -0,0 +1,56 @@
+#
+# Copyright (C) 2019-2020 Havoc-OS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Inherit some common Havoc stuff
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Official Havoc-OS
+export export HAVOC_BUILD_TYPE=Official
+
+# Inherit from X01BD device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := havoc_X01BD
+PRODUCT_DEVICE := X01BD
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := Zenfone Max Pro M2
+PRODUCT_MANUFACTURER := asus
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+TARGET_VENDOR := asus
+TARGET_VENDOR_PRODUCT_NAME := X01BD
+TARGET_VENDOR_DEVICE_NAME := X01BD
+
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=X01BD PRODUCT_NAME=X01BD
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="sdm660_64-user 9 PKQ1 1162 release-keys"
+
+BUILD_FINGERPRINT := "Android/sdm660_64/sdm660_64:9/PKQ1/16.2017.1905.065-20190507:user/release-keys"
+
+# Maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.havoc.maintainer=🔥SonalSingh18🔥
