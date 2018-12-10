67a8e256040c789b407c70173229d70f3f574b5f
Ethan Chen <intervigil@gmail.com>
2018-12-09 16:42:26 -0800

    X01BD: Use local DATA_IPACFG_MGR
    Change-Id: I6b074707510bde8fb001b42dbbe1654c6ebdb6ad

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -181,6 +181,9 @@ DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 # Init
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 
+# IPA
+USE_DEVICE_SPECIFIC_DATA_IPA_CFG_MGR := true
+
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
 
