6391b96f2c45c2cc8b11dd34f70875483d2d86a0
mike dooley <mdooley@google.com>
2018-12-18 20:29:02 +0100

    X01BD: Enabling sound trigger V2_2
    V2_2 has the same functionality as V2_1 except it adds one new
    api, getModelState.
    
    Note: V2_1 will be removed in a subsequent cl.
    
    Test: built android and tested using SoundTriggerTestApp
    
    Bug: 70206501
    Change-Id: Id2647687732e79df252588d69f95d2a407e0092c

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -20,7 +20,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.1-impl
+    android.hardware.soundtrigger@2.2-impl
 
 # Camera
 PRODUCT_PACKAGES += \
