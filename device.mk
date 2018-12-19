3c7bb69ea219db7e351b50d5b0dfd9f7b09d9b55
kubersharma001 <kubersharma001@gmail.com>
2018-12-19 22:12:41 +0530

    X01BD: import seccomp policy
    Change-Id: I42bb2c14dde4f97f1953c76b4536e81fcd673d1c

--- device.mk
+++ device.mk
@@ -91,6 +91,11 @@ PRODUCT_PACKAGES += \
     rcs_service_api \
     rcs_service_api.xml
 
+# Seccomp
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
