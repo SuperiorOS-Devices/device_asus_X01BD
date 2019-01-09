621eec7e5bb97920e42aa787a21f3ed38c6cd387
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-01-09 21:30:43 +0100

    X01BD: Build additional ril packages
    Change-Id: I3794cb8396f24a738bb74cdfd89a5394fd4651d3

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -81,9 +81,8 @@ PRODUCT_PACKAGES += \
 
 # Radio
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.0 \
-    android.hardware.radio@1.1 \
-    android.hardware.radio.deprecated@1.0
+    android.hardware.radio@1.2 \
+    android.hardware.radio.config@1.0
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
