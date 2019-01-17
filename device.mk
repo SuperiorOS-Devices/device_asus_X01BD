f91d75a87bac02915fd624823f1e5fe699e53f40
kubersharma001 <kubersharma001@gmail.com>
2019-01-17 20:31:35 +0530

    X01BD: import and add public.libraries.txt
    Change-Id: Iaed060aab705b2091f4258f93914a72473f43885

--- device.mk
+++ device.mk
@@ -228,6 +228,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
     $(LOCAL_PATH)/configs/msm_irqbalance_sdm630.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance_sdm630.conf
 
+# Public Libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # QTI whilelistapp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
