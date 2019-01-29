a90ee56f1fb5c43b1e545d60d99472404d3c67b1
kubersharma001 <kubersharma001@gmail.com>
2019-01-29 13:04:07 +0530

    X01BD: import keylayout from stock
    Change-Id: Id14d26517d0b6e0bfd49124bfd2b38cf801f1fd0

--- device.mk
+++ device.mk
@@ -180,6 +180,10 @@ PRODUCT_PACKAGES += \
     gps.conf \
     xtwifi.conf
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
