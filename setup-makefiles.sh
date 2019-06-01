256c56e464f8b5aedc57ff5fe56e24d3144e3283
Rahul Krishna <Rahulkrishna585@gmail.com>
2019-06-01 19:56:13 +0000

    X01BD: Split and cleanup proprietary files list
    * Remove unused blobs
    * from PKQ1.WW_Phone-16.2017.1905.065
    
    Change-Id: I6e2b5e9174395dc2fefdc51e43754951e5fe7a1f

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -43,6 +43,8 @@ setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
 write_headers
 
 write_makefiles "$MY_DIR"/proprietary-files.txt true
+echo "" >> "$PRODUCTMK"
+write_makefiles "$MY_DIR"/proprietary-files-qc.txt true
 
 # Finish
 write_footers
