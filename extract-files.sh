480399e67674e2cf43e5e96ce7c82ddee744961e
Rahul Krishna <Rahulkrishna585@gmail.com>
2019-06-01 19:56:13 +0000

    X01BD: Split and cleanup proprietary files list
    * Remove unused blobs
    * from PKQ1.WW_Phone-16.2017.1905.065
    
    Change-Id: I6e2b5e9174395dc2fefdc51e43754951e5fe7a1f

--- extract-files.sh
+++ extract-files.sh
@@ -59,4 +59,6 @@ setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
 
+extract "$MY_DIR"/proprietary-files-qc.txt "$SRC" "$SECTION"
+
 . "$MY_DIR"/setup-makefiles.sh
