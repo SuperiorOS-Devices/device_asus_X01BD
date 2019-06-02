55afd35b9a0b9177e4f484d810cfae071bdc2b32
Rahul Krishna <Rahulkrishna585@gmail.com>
2019-06-02 17:46:18 +0000

    X01BD: Build omx media hidl hal
    Change-Id: Ia05fe0b74feff52873abc1710bfcb4713a15741f

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -72,6 +72,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
 
+# Media
+PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-impl
+
 # Netutils
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.0
