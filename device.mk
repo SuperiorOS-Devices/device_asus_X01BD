dc82742416d025b6a0b9d66790ce64776ec6f549
kubersharma001 <kubersharma001@gmail.com>
2019-06-28 16:25:57 +0530

    X01BD: remove sensor_def_qcomdev.conf
     * already there in vendor
    
    Change-Id: I54e43c04faa7355dcd2fc55b8edf3041fbb655f7

--- device.mk
+++ device.mk
@@ -341,8 +341,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensor config
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
-    $(LOCAL_PATH)/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
+    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 # System properties
 -include $(LOCAL_PATH)/system_prop.mk
