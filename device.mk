08c69c16d0eb5fd5504d9575892e199b4d5bd818
Rahul Krishna <Rahulkrishna585@gmail.com>
2019-06-30 21:41:32 +0530

    X01BD: Cleanup and update overlays
    Change-Id: If66422213210c16a27e456f4ca8250266c398b7c

--- device.mk
+++ device.mk
@@ -238,7 +238,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # HIDL
 PRODUCT_PACKAGES += \
