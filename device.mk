7f2a02eb5a52ae42bbc36c1db4aaa3768a014fa8
Rahul Krishna <Rahulkrishna585@gmail.com>
2019-07-01 03:59:50 +0530

    X01BD: rootdir: Cleanup unused init scripts
    Change-Id: Ic23539993f356dbac367cc118655054305638572

--- device.mk
+++ device.mk
@@ -309,25 +309,12 @@ PRODUCT_PACKAGES += \
 
 # Ramdisk
 PRODUCT_PACKAGES += \
-    emmc_prop_init.sh \
     init.btmac.sh \
     init.class_main.sh \
-    init.crda.sh \
-    init.mdm.sh \
-    init.qcom.class_core.sh \
-    init.qcom.coex.sh \
-    init.qcom.crashdata.sh \
-    init.qcom.early_boot.sh \
-    init.qcom.efs.sync.sh \
     init.qcom.post_boot.sh \
-    init.qcom.sdio.sh \
     init.qcom.sensors.sh \
     init.qcom.sh \
-    init.qcom.usb.sh \
-    init.qti.fm.sh \
-    init.qti.ims.sh \
-    init.qti.qseecomd.sh \
-    qca6234-service.sh
+    init.qcom.usb.sh
 
 PRODUCT_PACKAGES += \
     fstab.qcom \
