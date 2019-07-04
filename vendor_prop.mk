7ab89d47c57e75e8f73864dcc3a4da4816bd8c55
Rizky Benggolo <travarilo@gmail.com>
2019-07-04 07:00:15 +0000

    X01BD: prop: Add some tweaks
    Change-Id: If0e5ac77caabda75d13d969bde662d2de03e35d5

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -102,6 +102,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.coresight.config=stm-events
 
+# Fling
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.min.fling_velocity=160 \
+    ro.max.fling_velocity=20000
+
 # FRP
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.frp.pst=/dev/block/bootdevice/by-name/frp
@@ -143,6 +148,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.disable_partial_split=1 \
     vendor.display.disable_rotator_downscale=1 \
     vendor.display.perf_hint_window=50 \
+    vendor.display.disable_rotator_downscale=1 \
+    debug.composition.type=c2d \
+    debug.mdpcomp.idletime=600 \
+    persist.hwc.mdpcomp.enable=true \
+    persist.hwc.ptor.enable=true \
+    debug.enable.sglscale=1 \
+    debug.egl.hw=1 \
+    debug.sf.disable_hwc=0 \
+    debug.sf.recomputecrop=0 \
+    debug.sf.disable_backpressure=1 \
+    debug.sf.gpu_comp_tiling=1 \
+    debug.performance.tuning=1 \
+    video.accelerate.hw=1
 
 # Maintainer
 PRODUCT_PROPERTY_OVERRIDES += \
