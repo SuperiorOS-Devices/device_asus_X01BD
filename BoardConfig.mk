4cdb461aee21e018963683b17ebe30f467ed05f8
Sonal Singh <sonal.singh.19993@gmail.com>
2019-07-12 13:41:12 +0000

    X01BD: Ignore Neverallows

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -259,6 +259,7 @@ VENDOR_SECURITY_PATCH := 2019-05-01
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_SEPOLICY_VERS := 28.0
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Tap to wake
 TARGET_TAP_TO_WAKE_NODE := /proc/tpd_gesture
