a940c10389cb37f7138207b037859ca21d940a45
Rahul Krishna <Rahulkrishna585@gmail.com>
2019-08-23 07:53:36 +0000

    X01BD: Add missing nfc config * Needed by secure_element hal

--- device.mk
+++ device.mk
@@ -227,6 +227,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
+    $(LOCAL_PATH)/configs/nfc/libese-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libese-nxp.conf
 
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
