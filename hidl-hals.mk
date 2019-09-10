020fc3a967fe5cc10c7171111435741b9cef8452
Rahul Krishna <Rahulkrishna585@gmail.com>
2019-09-10 13:05:47 +0530

    X01BD: Update overlays
    * Remove unsupported hidl hals
    
    Change-Id: I0e3a2cc814ca502dc8caa782d32814f7647e0d52

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -64,10 +64,6 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service
 
-# LiveDisplay native
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     android.hardware.media.omx@1.0-impl
@@ -106,10 +102,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
