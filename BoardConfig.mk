2d89f598b298468939e99d1ad5f37647074affc7
Michael Bestas <mkbestas@lineageos.org>
2019-10-01 21:14:38 +0300

    X01BD: Remove most rootdir symlinks and /dsp sepolicy
    * No longer needed with Q blobs
    * Keep /persist for now because cdfingerhal needs it.
    
    Change-Id: I3495fe5a3475c24c41eaedcb830fb12dfe395e44

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -149,10 +149,7 @@ PRODUCT_VENDOR_MOVE_ENABLED := true
 TARGET_COPY_OUT_VENDOR := vendor
 
 BOARD_ROOT_EXTRA_SYMLINKS := \
-    /mnt/vendor/persist:/persist \
-    /vendor/bt_firmware:/bt_firmware \
-    /vendor/dsp:/dsp \
-    /vendor/firmware_mnt:/firmware
+    /mnt/vendor/persist:/persist
 
 # Properties
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
