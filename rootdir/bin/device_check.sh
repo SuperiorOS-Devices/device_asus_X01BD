f06cdb546b8a9b4d838fd7dda44b6c2c1d21d414
Rahul Krishna <Rahulkrishna585@gmail.com>
2019-10-11 23:37:48 +0530

    X01BD: Fix device check script * Guess it works differently on M2 Pro * RU variant has NFC, rest doesn't ( i guess )
    Change-Id: I0f4f4b68ea9f0c72e96c596ffd10ba659b70160d

--- rootdir/bin/device_check.sh
+++ rootdir/bin/device_check.sh
@@ -15,7 +15,7 @@
 # limitations under the License.
 #
 # Check if device supports NFC
-if grep -q -w NFC /dev/block/platform/soc/c0c4000.sdhci/by-name/version; then
+if grep -q -w RU /mnt/vendor/persist/flag/countrycode.txt; then
     echo "Device supports NFC"
 else
     echo "Device doesn't support NFC , removing ..."
