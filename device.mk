34603a65f30c825b0a0681b500fb36e95cdca1dd
Ethan Chen <intervigil@gmail.com>
2019-10-20 01:17:32 -0700

    X01BD: Remove dead RCS packages
    Change-Id: Ie2c03fa11cb08c7ab900d7b666981d592aba19c7

--- device.mk
+++ device.mk
@@ -309,13 +309,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
-# RCS
-PRODUCT_PACKAGES += \
-    rcs_service_aidl \
-    rcs_service_aidl.xml \
-    rcs_service_api \
-    rcs_service_api.xml
-
 # Radio
 PRODUCT_PACKAGES += \
     rild \
