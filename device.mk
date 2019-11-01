81232ad2adc76f81118e009cdf668ed815d27570
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-01 07:19:07 -0500

    X01BD: Exclude lineage-sdk overlays from RRO
    * lineage-sdk overlays don't work with RRO
    
    Change-Id: Iff826faf1864388066a488acc9682636b98d1098

--- device.mk
+++ device.mk
@@ -36,6 +36,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 # Enforce RRO
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Permissions
 PRODUCT_COPY_FILES += \
