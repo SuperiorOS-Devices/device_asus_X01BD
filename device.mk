535b8c82427275e5675fb5f01dec85f80bf5464e
Sonal Singh <sonal.singh.19993@gmail.com>
2019-12-23 14:57:56 +0000

    X01BD: Parts: Rebrand to ZenParts

--- device.mk
+++ device.mk
@@ -429,6 +429,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
-# ZenfoneParts
+# ZenParts
 PRODUCT_PACKAGES += \
-    ZenfoneParts
+    ZenParts
