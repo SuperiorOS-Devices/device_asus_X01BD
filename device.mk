fa83df6cfacc27e9b1e95c54f3fa070fd357d948
Rahul Krishna <Rahulkrishna585@gmail.com>
2020-02-09 21:08:55 +0530

    X01BD: Remove powerhint.xml
    This reverts commit ceaa4a06cc9a5542aa7061269c3da12fc3c1b596.
    
    * We don't use this.
    
    Change-Id: I342b9e1a413bb29935af30c6651db14fbe97f020

--- device.mk
+++ device.mk
@@ -317,10 +317,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
 
-# Powerhint
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
-
 # Radio
 PRODUCT_PACKAGES += \
     rild \
