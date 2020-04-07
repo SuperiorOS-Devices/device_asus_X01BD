178f089d2744b8a683d7ec4bb250fe7395579b52
Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
2020-04-07 00:35:51 +0000

    Revert "X01BD: Build livedisplay 2.0"
    This reverts commit a1fead242bbb496d957e0dc2ebf57e4e1de0b48a.

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -85,10 +85,6 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service
 
-# LiveDisplay native
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     android.hardware.media.omx@1.0-impl
