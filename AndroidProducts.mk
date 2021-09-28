65e92c337728f0574669471dc6ef53dc0f8199e7
Shibi404 <shibisbs123@gmail.com>
2021-09-28 08:32:12 +0530

    X01BD: SuperiorOS Bringup
    Change-Id: I34937da12a73f7e885b2779735053c2a46e6025b

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,11 +1,13 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2021 The SuperiorOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X01BD.mk
+    $(LOCAL_DIR)/superior_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X01BD-userdebug
+    superior_X01BD-user \
+    superior_X01BD-userdebug \
+    superior_X01BD-eng
--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2021 The SuperiorOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -14,10 +14,7 @@ TARGET_SCREEN_WIDTH := 1080
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+    $(LOCAL_PATH)/overlay-sup
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
